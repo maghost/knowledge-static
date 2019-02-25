35260513c9d0d6fc88069e55c9fcf7c0053d291d
Matheus Marques <matheus_maghost@hotmail.com>
2019-02-24 22:10:00 -0300

    Teste modo navegação hash

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export const userKey = '__knowledge_user'
 export const baseApiUrl = 'http://mmarques-com-br.umbler.net'
-export const baseUrl = '/knowledge-static/'
+export const baseUrl = '/'
 
 export function showError(e) {
     if (e && e.response && e.response.data) {
